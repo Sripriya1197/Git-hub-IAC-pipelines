6e490123d6ca205b259f1b5815d97ae9e8804f45
Sri Priya <119073205+Sripriya1197@users.noreply.github.com>
2025-05-04 02:18:22 +0530

    Update main.tf

--- infrastructure/ecs-cluster/main.tf
+++ infrastructure/ecs-cluster/main.tf
@@ -1,39 +1,93 @@
-module "ecs_cluster" {
+module "ecs" {
   source = "terraform-aws-modules/ecs/aws"
-  cluster_name = "my-ecs-tf-cluster"
-}
 
-module "ecs_service" {
-  source          = "terraform-aws-modules/ecs/aws"
-  cluster         = module.ecs_cluster.cluster_id
-  name            = "my-ecs-sample-service"
-  desired_count   = 1
-  launch_type     = "FARGATE"
-  
-  # ECS service task definition
-  task_definitions = [
-    {
-      family = "my-ecs-sample-task"
-      container_definitions = jsonencode([{
-        name  = "my-container"
-        image = "273354669111.dkr.ecr.ap-south-1.amazonaws.com/github-action:1.1.1"
-        portMappings = [{
-          containerPort = 80
-          protocol      = "tcp"
-        }]
-      }])
-      cpu                      = 256
-      memory                   = 512
-      requires_compatibilities = ["FARGATE"]
-      network_mode             = "awsvpc"
+  cluster_name = "ecs-cluster-tf"
+
+  cluster_configuration = {
+    execute_command_configuration = {
+      logging = "OVERRIDE"
+      log_configuration = {
+        cloud_watch_log_group_name = "/aws/ecs/aws-ec2"
+      }
     }
-  ]
-  
-  network_configuration = {
-    subnets          = ["subnet-0697385b41cf20408"]
-    security_groups  = ["sg-0ef52138839aef07e"]
-    assign_public_ip = true
-    execution_role_arn = "arn:aws:iam::273354669111:role/ecsTaskExecution"
-    task_role_arn      = "arn:aws:iam::273354669111:role/ecsTaskExecution"
+  }
+
+  fargate_capacity_providers = {
+    FARGATE = {
+      default_capacity_provider_strategy = {
+        weight = 50
+      }
+    }
+    FARGATE_SPOT = {
+      default_capacity_provider_strategy = {
+        weight = 50
+      }
+    }
+  }
+
+  services = {
+    my-ecs-service = {
+      cpu    = 1024
+      memory = 4096
+
+      # Container definition(s)
+      container_definitions = {
+
+        fluent-bit = {
+          cpu       = 512
+          memory    = 1024
+          essential = true
+          image     = "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-ecs-service.html"
+          firelens_configuration = {
+            type = "fluentbit"
+          }
+          memory_reservation = 50
+        }
+
+        ecs-sample = {
+          cpu       = 512
+          memory    = 1024
+          essential = true
+          image     = "273354669111.dkr.ecr.ap-south-1.amazonaws.com/sample_app:1.0.0"
+          port_mappings = [
+            {
+              name          = "ecs-sample"
+              containerPort = 80
+              protocol      = "tcp"
+            }
+          ]
+
+          # Example image used requires access to write to root filesystem
+          readonly_root_filesystem = false
+
+          dependencies = [{
+            containerName = "fluent-bit"
+            condition     = "START"
+          }]
+
+      subnet_ids = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
+      security_group_rules = {
+        alb_ingress_3000 = {
+          type                     = "ingress"
+          from_port                = 80
+          to_port                  = 80
+          protocol                 = "tcp"
+          description              = "Service port"
+          source_security_group_id = "sg-12345678"
+        }
+        egress_all = {
+          type        = "egress"
+          from_port   = 0
+          to_port     = 0
+          protocol    = "-1"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      }
+    }
+  }
+
+  tags = {
+    Environment = "Development"
+    Project     = "sample-app"
   }
 }
