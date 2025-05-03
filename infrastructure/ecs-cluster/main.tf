2fd2dcf7d72acd9d50cde7ba7c7362c2e72e796c
Sri Priya <119073205+Sripriya1197@users.noreply.github.com>
2025-05-04 02:10:57 +0530

    Update main.tf

--- infrastructure/ecs-cluster/main.tf
+++ infrastructure/ecs-cluster/main.tf
@@ -3,38 +3,37 @@ module "ecs_cluster" {
   name   = "my-ecs-tf-cluster"
 }
 
-module "ecs_task_definition" {
-  source = "terraform-aws-modules/ecs/aws"
-  family = "my-ecs-sample-task"
-  container_definitions = jsonencode([{
-    name  = "my-container"
-    image = "273354669111.dkr.ecr.ap-south-1.amazonaws.com/sample_app:1.0.0"
-    portMappings = [{
-      containerPort = 80
-      protocol      = "tcp"
-    }] 
-  }])
-  cpu                      = 256
-  memory                   = 512
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-}
-
 module "ecs_service" {
-  source = "terraform-aws-modules/ecs/aws"
+  source          = "terraform-aws-modules/ecs/aws"
+  cluster         = module.ecs_cluster.cluster_id
+  name            = "my-ecs-sample-service"
+  desired_count   = 1
+  launch_type     = "FARGATE"
   
-  cluster_name = module.ecs_cluster.cluster_name
-
-  services = {
-    my_ecs_service = {
-      task_definition = module.ecs_task_definition.task_definition_arn
-      desired_count   = 1
-      launch_type     = "FARGATE"
-      network_configuration = {
-        subnets          = ["subnet-0697385b41cf20408"]
-        security_groups  = ["sg-0ef52138839aef07e"]
-        assign_public_ip = true
-      }
+  # ECS service task definition
+  task_definitions = [
+    {
+      family = "my-ecs-sample-task"
+      container_definitions = jsonencode([{
+        name  = "my-container"
+        image = "273354669111.dkr.ecr.ap-south-1.amazonaws.com/github-action:1.1.1"
+        portMappings = [{
+          containerPort = 80
+          protocol      = "tcp"
+        }]
+      }])
+      cpu                      = 256
+      memory                   = 512
+      requires_compatibilities = ["FARGATE"]
+      network_mode             = "awsvpc"
     }
+  ]
+  
+  network_configuration = {
+    subnets          = ["subnet-0697385b41cf20408"]
+    security_groups  = ["sg-0ef52138839aef07e"]
+    assign_public_ip = true
+    execution_role_arn = "arn:aws:iam::273354669111:role/ecsTaskExecution"
+    task_role_arn      = "arn:aws:iam::273354669111:role/ecsTaskExecution"
   }
 }
