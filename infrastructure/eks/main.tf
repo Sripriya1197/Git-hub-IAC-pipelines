387c62f5a687cb99f272cb018b6057c4d79f1365
Sri Priya <119073205+Sripriya1197@users.noreply.github.com>
2025-05-05 15:49:02 +0530

    Update main.tf

--- infrastructure/eks/main.tf
+++ infrastructure/eks/main.tf
@@ -3,6 +3,7 @@ module "eks" {
   cluster_name = "my-eks-tf-cluster"
   vpc_id       = "vpc-05601e20ad2591eb0"
   subnet_ids = ["subnet-0422988b659d1f0a1","subnet-0b09b067c081e2a26"]
+  control_plane_subnet_ids = ["subnet-0422988b659d1f0a1","subnet-0b09b067c081e2a26"]
   eks_managed_node_groups = {
     sample-app-tf = {
       instance_types = ["t3.medium"] 
