94f1ecf0e9bf8c77a9b47cf553372324d2e2f47c
Sri Priya <119073205+Sripriya1197@users.noreply.github.com>
2025-05-05 22:59:02 +0530

    Update main.tf

--- infrastructure/eks/main.tf
+++ infrastructure/eks/main.tf
@@ -5,9 +5,9 @@ module "eks" {
   cluster_endpoint_public_access = true
   enable_cluster_creator_admin_permissions = true
 
-  vpc_id                   = "vpc-05601e20ad2591eb0"
-  subnet_ids               = ["subnet-0422988b659d1f0a1", "subnet-0b09b067c081e2a26"]
-  control_plane_subnet_ids = ["subnet-01abbb296e6c29197", "subnet-0cc71f61342a9a205"]  
+  vpc_id                   = "vpc-0a19349c1563bf053"
+  subnet_ids               = ["subnet-01abbb296e6c29197", "subnet-0d97e9a39a2d34f30"]
+  #control_plane_subnet_ids = ["", ""]  
 
   eks_managed_node_group_defaults = {
     instance_types = ["t3.medium"]  
