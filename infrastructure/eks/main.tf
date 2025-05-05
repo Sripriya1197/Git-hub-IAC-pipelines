6a2ab1e0391ec0fd3fbff6b169a55d27380afaee
Sri Priya <119073205+Sripriya1197@users.noreply.github.com>
2025-05-05 23:21:19 +0530

    Update main.tf

--- infrastructure/eks/main.tf
+++ infrastructure/eks/main.tf
@@ -7,7 +7,7 @@ module "eks" {
 
   vpc_id                   = "vpc-0a19349c1563bf053"
   subnet_ids               = ["subnet-0cc71f61342a9a205", "subnet-0a433e5614138a125"]
-  control_plane_subnet_ids = ["subnet-0cc71f61342a9a205", "subnet-0a433e5614138a125"]  
+ 
 
   eks_managed_node_group_defaults = {
     instance_types = ["t3.medium"]  
