7559dc29c821af424c0544b4b615bad3457720a8
Sri Priya <119073205+Sripriya1197@users.noreply.github.com>
2025-05-05 23:38:53 +0530

    Update main.tf

--- infrastructure/eks/main.tf
+++ infrastructure/eks/main.tf
@@ -5,9 +5,8 @@ module "eks" {
   cluster_endpoint_public_access = true
   enable_cluster_creator_admin_permissions = true
 
-  vpc_id                   = "vpc-0a19349c1563bf053"
   subnet_ids               = ["subnet-0cc71f61342a9a205", "subnet-0a433e5614138a125"]
- 
+   vpc_id                   = "vpc-0a19349c1563bf053"
 
   eks_managed_node_group_defaults = {
     instance_types = ["t3.medium"]  
