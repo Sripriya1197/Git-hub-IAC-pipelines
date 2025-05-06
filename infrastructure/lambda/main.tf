895812d277be6d13576b0d60b04978aed76c979a
Sri Priya <119073205+Sripriya1197@users.noreply.github.com>
2025-05-06 23:14:29 +0530

    Update main.tf

--- infrastructure/lambda/main.tf
+++ infrastructure/lambda/main.tf
@@ -1,39 +1,9 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.4.0"
-    }
-  }
-}
-
-provider "docker" {
-  registry_auth {
-    address  = "273354669111.dkr.ecr.ap-south-1.amazonaws.com"
-    username = data.aws_ecr_authorization_token.token.user_name
-    password = data.aws_ecr_authorization_token.token.password
-  }
-}
-
-module "docker_image" {
-  source = "terraform-aws-modules/lambda/aws//modules/docker-build"
-  version = "~> 7.4"
-
-  create_ecr_repo = true
-  ecr_repo        = "my-lambda-ecr-repo"
-
-  source_path     = "."   # Make sure this points to the correct directory
-  image_tag       = "1.0"
-  use_image_tag   = true
-}
-
 module "lambda_function" {
   source ="terraform-aws-modules/lambda/aws"
   version = "~> 7.4"
 
-  function_name  = "my-docker-lambda"
+  function_name  = "my-f2c-lambda"
   create_package = false
   package_type   = "Image"
-  image_uri      = module.docker_image.image_uri
-
+  image_uri      = "273354669111.dkr.ecr.ap-south-1.amazonaws.com/lambda:1.0.0"
 }
