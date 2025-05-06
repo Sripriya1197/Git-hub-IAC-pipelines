2d83680f70c95883a8f13c890daa56defab69334
Sri Priya <119073205+Sripriya1197@users.noreply.github.com>
2025-05-06 20:38:47 +0530

    Update main.tf

--- infrastructure/lambda/main.tf
+++ infrastructure/lambda/main.tf
@@ -1,7 +1,8 @@
+# IAM role for Lambda execution (using terraform-aws-modules/iam/aws module)
 module "lambda_role" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-role"
+  source = "terraform-aws-modules/iam/aws"
 
-  role_name = "my-lambda-execution-role"
+  name = "my-lambda-execution-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -56,20 +57,20 @@ module "eventbridge_rule" {
 
 # EventBridge target to invoke Lambda (using terraform-aws-modules/eventbridge/aws module)
 module "eventbridge_target" {
-  source = "terraform-aws-modules/eventbridge/aws//modules/target-lambda"
+  source = "terraform-aws-modules/eventbridge/aws"
 
   rule       = module.eventbridge_rule.name
   target_id  = "LambdaTarget"
-  lambda_arn = module.lambda_function.function_arn
+  arn        = module.lambda_function.function_arn
   input      = jsonencode({
     "key" = "value"
   })
 }
 
 # Lambda permission for EventBridge to invoke the Lambda function (using terraform-aws-modules/lambda/aws module)
-module "lambda_permission" {
-  source = "terraform-aws-modules/lambda/aws//modules/permission"
-
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
   function_name = module.lambda_function.function_name
   principal     = "events.amazonaws.com"
   source_arn    = module.eventbridge_rule.arn
