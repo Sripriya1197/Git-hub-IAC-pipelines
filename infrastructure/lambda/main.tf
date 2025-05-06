c17d52042e62cc5f5fd99138920fd4bb47bdf143
Sri Priya <119073205+Sripriya1197@users.noreply.github.com>
2025-05-06 20:26:24 +0530

    Update main.tf

--- infrastructure/lambda/main.tf
+++ infrastructure/lambda/main.tf
@@ -23,7 +23,8 @@ module "lambda_role" {
           Action   = [
             "logs:*",
             "cloudwatch:*",
-            "xray:*"
+            "xray:*",
+            "apigateway:*" # Add API Gateway permissions
           ]
           Effect   = "Allow"
           Resource = "*"
@@ -32,6 +33,8 @@ module "lambda_role" {
     }
   ]
 }
+
+# Lambda Function Module (using Docker image from ECR)
 module "lambda_function" {
   source        = "git::https://github.com/Sripriya1197/terraform-module.git//.modules/aws/lambda?ref=main"
   function_name = "my-lambda-function"
@@ -44,4 +47,38 @@ module "lambda_function" {
   environment_variables = {
     ENV = "dev"
   }
+}
+
+# API Gateway for triggering Lambda
+resource "aws_api_gateway_rest_api" "lambda_api" {
+  name        = "lambda-api"
+  description = "API for Lambda function"
+}
+
+resource "aws_api_gateway_resource" "lambda_resource" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  parent_id   = aws_api_gateway_rest_api.lambda_api.root_resource_id
+  path_part   = "trigger"
+}
+
+resource "aws_api_gateway_method" "lambda_method" {
+  rest_api_id   = aws_api_gateway_rest_api.lambda_api.id
+  resource_id   = aws_api_gateway_resource.lambda_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  resource_id = aws_api_gateway_resource.lambda_resource.id
+  http_method = aws_api_gateway_method.lambda_method.http_method
+  integration_http_method = "POST"
+  type = "AWS_PROXY"
+  uri  = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/${module.lambda_function.lambda_function_arn}/invocations"
+}
+
+resource "aws_lambda_permission" "allow_api_gateway" {
+  action        = "lambda:InvokeFunction"
+  function_name = module.lambda_function.lambda_function_name
+  principal     = "apigateway.amazonaws.com"
 }
