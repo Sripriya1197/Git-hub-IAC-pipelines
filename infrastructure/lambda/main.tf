e9b51a24a9b1784b7984f4140488fdca879ce070
Sri Priya <119073205+Sripriya1197@users.noreply.github.com>
2025-05-06 22:24:57 +0530

    Update main.tf

--- infrastructure/lambda/main.tf
+++ infrastructure/lambda/main.tf
@@ -14,21 +14,22 @@ provider "docker" {
     password = data.aws_ecr_authorization_token.token.password
   }
 }
+
+data "aws_ecr_authorization_token" "token" {}
+
 module "docker_image" {
   source = "terraform-aws-modules/lambda/aws//modules/docker-build"
-  version = "~> 7.4"
-
+  
   create_ecr_repo = true
   ecr_repo        = "my-lambda-ecr-repo"  
 
-  source_path     = "."       
+  source_path     = "."  # Assuming Dockerfile is in the same directory as main.tf
   image_tag       = "1.0"
   use_image_tag   = true
 }
 
 module "lambda_function" {
-  source = "git::https://github.com/Sripriya1197/terraform-module.git//.modules/aws/lambda?ref=main"
-  version = "~> 7.4"
+  source = "git::https://github.com/Sripriya1197/terraform-module.git//modules/aws/lambda?ref=main"
 
   function_name  = "my-docker-lambda"
   create_package = false
