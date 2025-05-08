969c6e25920ad7ec09b28874270223a2ff8614e6
Sri Priya <119073205+Sripriya1197@users.noreply.github.com>
2025-05-09 00:00:31 +0530

    Update variables.tf

--- infrastructure/ecs-cluster/variables.tf
+++ infrastructure/ecs-cluster/variables.tf
@@ -8,3 +8,8 @@ variable "env" {
   description = "Environment name like dev, stage, prod"
   default     =""
 }
+variable "cloudwatch_log_group_name" {
+  description = "Custom name of CloudWatch Log Group for ECS cluster"
+  type        = string
+  default     = null
+}
