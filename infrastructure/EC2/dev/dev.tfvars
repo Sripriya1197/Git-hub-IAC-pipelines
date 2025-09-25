06c806514d91f5e0ad7fe886884e374b108f5ecb
Sripriya1197 <rsripriyadharshini@gmail.com>
2025-09-25 22:28:48 +0530

    updated

--- infrastructure/EC2/dev/dev.tfvars
+++ infrastructure/EC2/dev/dev.tfvars
@@ -1,6 +1,8 @@
 
-     ami                =   ami-0360c520857e3138f
-     key_name           =   WS-SAMPLE
-     instance_type      =   t3.micro
-     subnet_id          =   subnet-005c36e0d7ffa7995
-     vpc_security_group_ids = ["sg-05f766915783bfa1e"]
+ami = "ami-0360c520857e3138f"
+key_name = "AWS-SAMPLE"
+instance_type = "t3.micro"
+subnet_id = "subnet-005c36e0d7ffa7995"
+vpc_security_group_ids = ["sg-05f766915783bfa1e"]
+name = "my-demo-ec2"
+tags = { Env = "dev" }
--- infrastructure/EC2/dev/main.tf
+++ infrastructure/EC2/dev/main.tf
@@ -1,19 +1,14 @@
 provider "aws" {
-  region = "ap-south-1" 
+  region = "ap-south-1"
 }
+
 module "ec2_instance" {
-   source = "git::https://github.com/Sripriya1197/terraform-module.git//.modules/aws/ec2?ref=main"
+  source = "git::https://github.com/Sripriya1197/terraform-module.git//.modules/aws/ec2?ref=main"
 
-  //   count              =   var.count 
-     ami                =   var.ami
-     key_name           =   var.key_name
-     instance_type      =   var.instance_type
-     subnet_id          =   var.subnet_id
-     vpc_security_group_ids = var.vpc_security_group_ids
-
-   tags ={
-     Name = var.instance_name
-   }
-
+  ami                    = var.ami
+  key_name               = var.key_name
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.vpc_security_group_ids
+  tags                   = merge(var.tags, { Name = var.name })
 }
-
