1ea98494b1113d883b69b59898328606aa9bf9ae
Sri Priya <119073205+Sripriya1197@users.noreply.github.com>
2025-09-25 22:00:51 +0530

    Update main.tf

--- infrastructure/EC2/dev/main.tf
+++ infrastructure/EC2/dev/main.tf
@@ -5,18 +5,15 @@ module "ec2_instance" {
    source = "git::https://github.com/Sripriya1197/terraform-module.git//.modules/aws/ec2?ref=main"
 
   //   count              =   var.count      
-     region             =   var.region
-     ami                =   var.region
-     key_name           =   var.key_name
-     instance_type      =   var.instance_type
-     subnet_id          =   var.subnet_id
-     vpc_security_group_ids = var.vpc_security_group_ids
-
-   tags ={
-     Name = var.instance_name
-   }
+    ami     = var.ami_id
+  key_name = var.key_name
+  instance_type = var.instance_type
+  subnet_id = var.subnet_id
+  vpc_security_group_ids = var.vpc_security_group_ids
+  tags = { Name = var.instance_name }
 
 }
+
 
 
 
