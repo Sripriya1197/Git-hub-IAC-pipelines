cf5a1f90e18946439ed1d6d437e59d69220781a6
Sripriya1197 <rsripriyadharshini@gmail.com>
2025-09-29 22:00:48 +0530

    updated file

---
+++ infrastructure/EC2/dev/variables.tf
@@ -0,0 +1,35 @@
+variable "name" {
+  description = "Name to be used on EC2 instance created"
+  type        = string
+  default     = ""
+}
+variable "ami" {
+  description = "ID of AMI to use for the instance"
+  type        = string
+  default     = null
+}
+variable "instance_type" {
+  description = "The type of instance to start"
+  type        = string
+  default     = "t3.micro"
+}
+variable "key_name" {
+  description = "Key name of the Key Pair to use for the instance; which can be managed using the `aws_key_pair` resource"
+  type        = string
+  default     = null
+}
+variable "subnet_id" {
+  description = "The VPC Subnet ID to launch in"
+  type        = string
+  default     = null
+}
+
+variable "vpc_security_group_ids" {
+  description = "A list of security group IDs to associate with"
+  type        = list(string)
+  default     = []
+}
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
